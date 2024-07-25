44ccbd1e3c0bd1ecf5b6678ddf3b7b2953247ce7
EricOrtiz28 <eric.ortiz.l@uni.pe>
2024-07-25 16:24:56 -0500

    segundo commit

--- src/app/service/usuarios.service.ts
+++ src/app/service/usuarios.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { BASE_URL } from '../utils/backend-config';
 
 @Injectable({
@@ -12,6 +13,32 @@ export class UsuariosService {
   constructor(private http: HttpClient) { }
 
   insertUsuario(usuario: any): Observable<any> {
-    return this.http.post<any>(this.apiUrlUsuariosInsert, usuario);
+    return this.getUserIP().pipe(
+      switchMap(ip => this.getUserLocation(ip).pipe(
+        map(locacion => ({
+          ...usuario,
+          ip,
+          locacion,
+          dispositivo: this.getUserDevice()
+        })),
+        switchMap(usuarioCompleto => this.http.post<any>(this.apiUrlUsuariosInsert, usuarioCompleto))
+      ))
+    );
+  }
+
+  private getUserIP(): Observable<string> {
+    return this.http.get<{ ip: string }>('https://api64.ipify.org?format=json').pipe(
+      map(response => response.ip)
+    );
+  }
+
+  private getUserLocation(ip: string): Observable<string> {
+    return this.http.get<any>(`https://ipapi.co/${ip}/json/`).pipe(
+      map(response => `${response.city}, ${response.country_name}`)
+    );
+  }
+
+  private getUserDevice(): string {
+    return navigator.userAgent;
   }
 }
